1d8431673881d922825943301873756dbc2d1664
Dmytro2708 <130864203+Dmytro2708@users.noreply.github.com>
2023-09-12 22:28:46 +0300

    add task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+let timerId = null;
+btnStop.disabled = true;
+
+btnStart.addEventListener('click', onClickStart);
+btnStop.addEventListener('click', onClickStop);
+
+function onClickStart(evt) {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function onClickStop(evt) {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
